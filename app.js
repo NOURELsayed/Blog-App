979cdcb5ebc274b717de9d8f9ceab82ca453f94a
NOURELsayed <nourelsayed538@gmail.com>
2022-03-09 16:59:57 +0200

    start the app

---
+++ app.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv");
+
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 5000;
+dotenv.config({ path: "./config/config.env" });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`)
+});
